1e1353bb11d5b6baeb24edd54477a3c68f92357d
muyinda <you@example.com>
2019-01-23 15:26:55 +0300

    on it

---
+++ try.py
@@ -0,0 +1,11 @@
+print('hello!')
+
+children = ['emma':'dark', 'sandie': 'light']
+    print(children)
+age= int(input('Enter your age: '))    
+if age>18:
+    print('its okay to gamble')
+elif age = 18:
+    print('your authorised too, to gamble')
+else:
+    print('very young to start gambling')    
